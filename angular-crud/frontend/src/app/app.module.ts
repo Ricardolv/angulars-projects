3c23b61316930cb6c22ddd2671970b3743715fea
Ricardo Viana <richardluizv@gmail.com>
2022-01-27 18:51:02 -0300

    implementando components upsate e delete

--- angular-crud/frontend/src/app/app.module.ts
+++ angular-crud/frontend/src/app/app.module.ts
@@ -33,6 +33,8 @@ import { ForDirective } from './directives/for.directive';
 import { ProductsComponent } from './views/products/products.component';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
 import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 
 registerLocaleData(localePt);
@@ -50,6 +52,8 @@ registerLocaleData(localePt);
     ProductsComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
