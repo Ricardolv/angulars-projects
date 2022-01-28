3de2daa1db4ad8d8c6dc7defde9081851d16e804
Ricardo Viana <richardluizv@gmail.com>
2022-01-28 01:02:18 -0300

    Lista de Cursos: Pipe para mostrar ícone

--- application-ui/src/app/shared/shared.module.ts
+++ application-ui/src/app/shared/shared.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipes/category.pipe';
+
 
 
 
 @NgModule({
   declarations: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe
   ],
   imports: [
     CommonModule,
     AppMaterialModule
   ],
   exports: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe
   ]
 })
 export class SharedModule { }
