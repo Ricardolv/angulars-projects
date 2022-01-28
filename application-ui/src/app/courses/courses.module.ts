18602451ca601bce1db071534eedd05c02668852
Ricardo Viana <richardluizv@gmail.com>
2022-01-27 23:50:01 -0300

    Criando Material Table para Listar Cursos

--- application-ui/src/app/courses/courses.module.ts
+++ application-ui/src/app/courses/courses.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from './../shared/shared.module';
 import { CoursesRoutingModule } from './courses-routing.module';
