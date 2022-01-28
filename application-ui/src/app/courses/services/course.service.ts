dd961befab11123106fa98cd1f4a8e29482463d7
Ricardo Viana <richardluizv@gmail.com>
2022-01-28 02:39:33 -0300

    add proxy.config

--- application-ui/src/app/courses/services/course.service.ts
+++ application-ui/src/app/courses/services/course.service.ts
@@ -9,7 +9,7 @@ import { Course } from '../model/course';
 })
 export class CourseService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
